c16b6ef561b90d21a52eb08854235aa9a5dd95bf
mdelage <mdelage@student.42.fr>
2015-04-28 19:13:41 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Default install
+DEFAULT=true
 # LN contain files which will get a symlink
 LN=("aliases" "ls_colors" "vim" "vimrc" "zshrc")
 # DEST is the destination dir
@@ -17,11 +19,13 @@ function usage()
     done
     echo ""
     echo "Suported options :"
+    echo -e "\t-a|--available-files : list all available file on which you can crete symlink"
     echo -e "\t-c|--clean : delete all symlink and restore previous file if any"
     echo -e "\t-e|--existing : list all existing symlink"
     echo -e "\t-f|--files file1,file2 : give a list a files comma separated"
-    echo -e "\t-l|--list : list all available file on which you can crete symlink"
     echo -e "\t-h|--help : display this help"
+	echo -e "\t-l|--symlink: update symlink"
+	echo -e "\t-s|--submodules : init and update submodules"
 	echo -e "\t-g|--geam : my personnal config /!\\ DO NOT USE IT"
 	echo -e "\t--g42 : my personnal config for 42 /!\\ DO NOT USE IT"
     exit 0
@@ -74,69 +78,88 @@ function get_files_list()
 
 # get the arg of the script
 while test $# -gt 0; do
-    case "$1" in
-        -h|--help)
-            usage $0
-            ;;
-        -c|--clean)
-            rm_ln
-            exit 0
-            ;;
-        -l|--list)
-            ls -l ${SRC}
-            exit 0
-            ;;
-        -e|--existing)
-            existing
-            exit 0
-            ;;
-        -f|--files)
-            shift
-            get_files_list $1
-            ;;
-        -g|--geam)
-            GEAM=true
-            LINUX=true
-            ;;
-        --g42)
-            GEAM=true
-            G42=true
-            ;;
-        *)
-            usage $0
-            ;;
-    esac
-    shift
+	case "$1" in
+		-h|--help)
+			usage $0
+			;;
+		-c|--clean)
+			DELETE=true
+			;;
+		-a|--available_files)
+			ls -l ${SRC}
+			exit 0
+			;;
+		-e|--existing)
+			existing
+			exit 0
+			;;
+		-f|--files)
+			shift
+			get_files_list $1
+			;;
+		-s|--submodules)
+			if [[ -n "${DEFAULT}" ]]; then
+				unset DEFAULT
+			fi
+			SUBMODULES=true
+			;;
+		-l|--symlink)
+			if [[ -n "${DEFAULT}" ]]; then
+				unset DEFAULT
+			fi
+			SYMLINK=true
+			;;
+		-g|--geam)
+			GEAM=true
+			LINUX=true
+			;;
+		--g42)
+			GEAM=true
+			G42=true
+			;;
+		*)
+			usage $0
+			;;
+	esac
+	shift
 done
 
-# if it's me, add the git config
-if [[ -n "${GEAM}" ]]; then
-    LN+=("gitconfig" "gitignore_global")
+if [[ -n "${DELETE}" ]] || [[ -n "${SYMLINK}" ]]; then
+	rm_ln
 fi
 
-# if it's me on linux
-if [[ -n "${LINUX}" ]]; then
-    LN+=("irssi" "tmux" "tmux.config")
+if [[ -n "${DEFAULT}" ]] || [[ -n "${SYMLINK}" ]]; then
+	# if it's me, add the git config
+	if [[ -n "${GEAM}" ]]; then
+		LN+=("gitconfig" "gitignore_global")
+	fi
+
+	# if it's me on linux
+	if [[ -n "${LINUX}" ]]; then
+		LN+=("irssi" "tmux" "tmux.config")
+	fi
+
+	do_ln
 fi
 
-do_ln
-
-cd ${SRC}
-git submodule init
-git submodule update
+if [[ -n "${DEFAULT}" ]] || [[ -n "${SUBMODULES}" ]]; then
+	cd ${SRC}
+	git submodule init
+	git submodule update
+fi
 
 if [[ -n "${G42}" ]]; then
-    cd $HOME
+	cd $HOME
 	if [[ ! -d "$HOME/libft" ]]; then
-	    git clone git@github.com:Geam/libft.git libft
+		git clone git@github.com:Geam/libft.git libft
 	fi
 	if [[ ! -d "$HOME/scripts" ]]; then
-	    git clone git@github.com:Geam/scripts.git scripts
+		git clone git@github.com:Geam/scripts.git scripts
 	fi
 	if [[ ! -L "$HOME/sgoinfre" ]]; then
-	    ln -s /nfs/sgoinfre
+		ln -s /nfs/sgoinfre
 	fi
-    brew update
-    source $HOME/.zshrc
-    brew install htop tig
+	brew update
+	source $HOME/.zshrc
+	brew install htop tig
 fi
