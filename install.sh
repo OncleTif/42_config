1b454bd8e064a33a2c70f93da6fe355c749981bd
mdelage <mdelage@student.42.fr>
2015-04-28 22:14:20 +0200

    correct tabulation in install.sh

--- install.sh
+++ install.sh
@@ -24,10 +24,10 @@ function usage()
     echo -e "\t-e|--existing : list all existing symlink"
     echo -e "\t-f|--files file1,file2 : give a list a files comma separated"
     echo -e "\t-h|--help : display this help"
-	echo -e "\t-l|--symlink: update symlink"
-	echo -e "\t-s|--submodules : init and update submodules"
-	echo -e "\t-g|--geam : my personnal config /!\\ DO NOT USE IT"
-	echo -e "\t--g42 : my personnal config for 42 /!\\ DO NOT USE IT"
+    echo -e "\t-l|--symlink: update symlink"
+    echo -e "\t-s|--submodules : init and update submodules"
+    echo -e "\t-g|--geam : my personnal config /!\\ DO NOT USE IT"
+    echo -e "\t--g42 : my personnal config for 42 /!\\ DO NOT USE IT"
     exit 0
 }
 
@@ -78,89 +78,89 @@ function get_files_list()
 
 # get the arg of the script
 while test $# -gt 0; do
-	case "$1" in
-		-h|--help)
-			usage $0
-			;;
-		-c|--clean)
-			DELETE=true
-			unset DEFAULT
-			;;
-		-a|--available_files)
-			ls -l ${SRC}
-			exit 0
-			;;
-		-e|--existing)
-			existing
-			exit 0
-			;;
-		-f|--files)
-			shift
-			get_files_list $1
-			;;
-		-s|--submodules)
-			if [[ -n "${DEFAULT}" ]]; then
-				unset DEFAULT
-			fi
-			SUBMODULES=true
-			;;
-		-l|--symlink)
-			if [[ -n "${DEFAULT}" ]]; then
-				unset DEFAULT
-			fi
-			SYMLINK=true
-			;;
-		-g|--geam)
-			GEAM=true
-			LINUX=true
-			;;
-		--g42)
-			GEAM=true
-			G42=true
-			;;
-		*)
-			usage $0
-			;;
-	esac
-	shift
+    case "$1" in
+        -h|--help)
+            usage $0
+            ;;
+        -c|--clean)
+            DELETE=true
+            unset DEFAULT
+            ;;
+        -a|--available_files)
+            ls -l ${SRC}
+            exit 0
+            ;;
+        -e|--existing)
+            existing
+            exit 0
+            ;;
+        -f|--files)
+            shift
+            get_files_list $1
+            ;;
+        -s|--submodules)
+            if [[ -n "${DEFAULT}" ]]; then
+                unset DEFAULT
+            fi
+            SUBMODULES=true
+            ;;
+        -l|--symlink)
+            if [[ -n "${DEFAULT}" ]]; then
+                unset DEFAULT
+            fi
+            SYMLINK=true
+            ;;
+        -g|--geam)
+            GEAM=true
+            LINUX=true
+            ;;
+        --g42)
+            GEAM=true
+            G42=true
+            ;;
+        *)
+            usage $0
+            ;;
+    esac
+    shift
 done
 
 if [[ -n "${DELETE}" ]] || [[ -n "${SYMLINK}" ]]; then
-	rm_ln
+    rm_ln
 fi
 
 if [[ -n "${DEFAULT}" ]] || [[ -n "${SYMLINK}" ]]; then
-	# if it's me, add the git config
-	if [[ -n "${GEAM}" ]]; then
-		LN+=("gitconfig" "gitignore_global")
-	fi
+    # if it's me, add the git config
+    if [[ -n "${GEAM}" ]]; then
+        LN+=("gitconfig" "gitignore_global")
+    fi
 
-	# if it's me on linux
-	if [[ -n "${LINUX}" ]]; then
-		LN+=("irssi" "tmux" "tmux.config")
-	fi
+    # if it's me on linux
+    if [[ -n "${LINUX}" ]]; then
+        LN+=("irssi" "tmux" "tmux.config")
+    fi
 
-	do_ln
+    do_ln
 fi
 
 if [[ -n "${DEFAULT}" ]] || [[ -n "${SUBMODULES}" ]]; then
-	cd ${SRC}
-	git submodule init
-	git submodule update
+    cd ${SRC}
+    git submodule init
+    git submodule update
 fi
 
 if [[ -n "${G42}" ]]; then
-	cd $HOME
-	if [[ ! -d "$HOME/libft" ]]; then
-		git clone git@github.com:Geam/libft.git libft
-	fi
-	if [[ ! -d "$HOME/scripts" ]]; then
-		git clone git@github.com:Geam/scripts.git scripts
-	fi
-	if [[ ! -L "$HOME/sgoinfre" ]]; then
-		ln -s /nfs/sgoinfre
-	fi
-	brew update
-	source $HOME/.zshrc
-	brew install htop tig
+    cd $HOME
+    if [[ ! -d "$HOME/libft" ]]; then
+        git clone git@github.com:Geam/libft.git libft
+    fi
+    if [[ ! -d "$HOME/scripts" ]]; then
+        git clone git@github.com:Geam/scripts.git scripts
+    fi
+    if [[ ! -L "$HOME/sgoinfre" ]]; then
+        ln -s /nfs/sgoinfre
+    fi
+    brew update
+    source $HOME/.zshrc
+    brew install htop tig
 fi
